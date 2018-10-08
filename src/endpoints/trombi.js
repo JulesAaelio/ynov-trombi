ab107fbc469f953250d96d2210d8f49f94c58c05
Jules LAURENT <jules.laurent@ynov.com>
2018-10-08 10:23:46 +0200

    set data fetching from db

--- src/endpoints/trombi.js
+++ src/endpoints/trombi.js
@@ -1,5 +1,8 @@
 module.exports = (app,db) => {
-  app.get('/trombi',(req,res) => {
-    res.render('base');
+  app.get('/trombi',async (req,res) => {
+      const persons = await db.Person.findAll();
+    res.render('trombi', {
+      persons,
+    });
   });
 };
