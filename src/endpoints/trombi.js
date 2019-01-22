b1fe92ba3bf7b82041395d9a7fe319aef5ee7446
Jules LAURENT <jules.laurent@ynov.com>
2019-01-22 17:19:16 +0100

    add query parameters filters

--- src/endpoints/trombi.js
+++ src/endpoints/trombi.js
@@ -1,6 +1,28 @@
 module.exports = (app,db) => {
   app.get('/trombi',async (req,res) => {
-      const persons = await db.Person.findAll();
+      const options = {};
+      if(req.query.spe) {
+          options.where  = Object.assign(options.where || {}, {
+              speciality: {
+                  [db.db.Op.eq] : req.query.spe.toLowerCase()
+              }
+          });
+      }
+      if(req.query.firstname) {
+          options.where  = Object.assign(options.where || {}, {
+              firstname: {
+                  [db.db.Op.like] : `%${req.query.firstname}%`
+              }
+          });
+      }
+      if(req.query.lastname) {
+          options.where  = Object.assign(options.where || {}, {
+              lastname: {
+                  [db.db.Op.like] : `%${req.query.lastname}%`
+              }
+          });
+      }
+      const persons = await db.Person.findAll(options);
     res.render('trombi', {
       persons,
     });
